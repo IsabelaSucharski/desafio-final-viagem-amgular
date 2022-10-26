3e390108fca07e52272cc9cb4d27197c973a21a9
Isabela Sucharski <isabela.sucharski@brf.com>
2022-10-26 16:59:57 -0300

    calculo valor total

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,13 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  ValidatorFn,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 import { CountriesService } from './countries.service';
-
-interface Viagem {
-  paisOrigem: string;
-  cidadeOrigem: string;
-  paisDestino: string;
-  cidadeDestino: string;
-  distancia: string;
-  adultos: string;
-  criancas: string;
-  tipoDeVoo: string;
-  milhas: string;
-  valorAdulto: string;
-  valorCrianca: string;
-  valorAbatidoMilhas: string;
-  total: string;
-}
 
 interface Cities {
   city: string;
@@ -37,22 +27,53 @@ interface Coutries {
 export class AppComponent implements OnInit {
   countries: Coutries[] = [];
   cidadesOrigem: Cities[] = [];
+  latitudeOrigem: any = '';
+  longitudeOrigem: any = '';
   cidadesDestino: Cities[] = [];
-  paises: string = '';
-  // classe: string = '';
+  latitudeDestino: any = '';
+  longitudeDestino: any = '';
   criancas: number = 0;
-  adultos: number = 0;
+  adultos: number = 1;
+  distanciaViagem: number = 0;
+  valorAbatidoMilhas: any = '';
+  valorAdulto: any = '';
+  valorCrianca: any = '';
+  totalViagem: any = '';
 
   constructor(private countriesService: CountriesService) {}
-  myForm = new FormGroup({
-    paisOrigem: new FormControl('Brasil'),
-    cidadeOrigem: new FormControl('São Paulo'),
-    paisDestino: new FormControl('Canadá'),
-    cidadeDestino: new FormControl('Ottawa'),
-    classe: new FormControl('economica'),
-    adultos: new FormControl(0),
-    criancas: new FormControl(0),
-  });
+
+  validateMinAdults: ValidatorFn = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    const adultos = control.get('adultos')?.value;
+    return adultos < 1 ? { errorMinAdults: true } : null;
+  };
+
+  validateCitiesOriginDestiny: ValidatorFn = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    const cidadeOrigem = control.get('cidadeOrigem')?.value;
+    const cidadeDestino = control.get('cidadeDestino')?.value;
+    return cidadeOrigem === cidadeDestino
+      ? { errorCitiesOriginDestiny: true }
+      : null;
+  };
+
+  myForm = new FormGroup(
+    {
+      paisOrigem: new FormControl('Brasil'),
+      cidadeOrigem: new FormControl('São Paulo'),
+      paisDestino: new FormControl('Canadá'),
+      cidadeDestino: new FormControl('Ottawa'),
+      classe: new FormControl('Econômica'),
+      adultos: new FormControl(1),
+      criancas: new FormControl(0),
+      milhas: new FormControl(15000),
+    },
+    {
+      validators: [this.validateMinAdults, this.validateCitiesOriginDestiny],
+    }
+  );
 
   ngOnInit(): void {
     this.setValues();
@@ -68,6 +89,15 @@ export class AppComponent implements OnInit {
 
       filtroPaisOrigem.filter(({ cities }: any) => {
         this.cidadesOrigem = cities;
+
+        let cidade = this.cidadesOrigem.filter(({ city }) => {
+          return city === this.myForm.get('cidadeOrigem')?.value;
+        });
+
+        cidade.map(({ latitude, longitude }) => {
+          this.latitudeOrigem = latitude;
+          this.longitudeOrigem = longitude;
+        });
       });
 
       let filtroPaisDestino = country.filter((c: any) => {
@@ -76,11 +106,22 @@ export class AppComponent implements OnInit {
 
       filtroPaisDestino.filter(({ cities }: any) => {
         this.cidadesDestino = cities;
+
+        let cidade = this.cidadesDestino.filter(({ city }) => {
+          return city === this.myForm.get('cidadeDestino')?.value;
+        });
+
+        cidade.map(({ latitude, longitude }) => {
+          this.latitudeDestino = latitude;
+          this.longitudeDestino = longitude;
+        });
       });
     });
   };
 
   origem = (value: any) => {
+    this.myForm.patchValue({ cidadeOrigem: '' });
+
     let filtroPaisOrigem = this.countries.filter((c: any) => {
       return c.country === value;
     });
@@ -90,7 +131,40 @@ export class AppComponent implements OnInit {
     });
   };
 
+  setValuesOrigem = (valor: any) => {
+    let cidade = this.cidadesOrigem.filter(({ city }) => {
+      return city === valor;
+    });
+
+    cidade.map(({ latitude, longitude }) => {
+      this.latitudeOrigem = latitude;
+      this.longitudeOrigem = longitude;
+    });
+  };
+
+  setValuesDestino = (valor: any) => {
+    let cidade = this.cidadesDestino.filter(({ city }) => {
+      return city === valor;
+    });
+
+    cidade.map(({ latitude, longitude }) => {
+      this.latitudeDestino = latitude;
+      this.longitudeDestino = longitude;
+
+      this.distanciaViagem = this.getDistance(
+        this.latitudeOrigem,
+        this.longitudeOrigem,
+        this.latitudeDestino,
+        this.longitudeDestino
+      );
+
+      this.setPrecoViagem();
+    });
+  };
+
   destino = (value: any) => {
+    this.myForm.patchValue({ cidadeDestino: '' });
+
     let filtroPaisDestino = this.countries.filter((c: any) => {
       return c.country === value;
     });
@@ -109,6 +183,7 @@ export class AppComponent implements OnInit {
       this.myForm.patchValue({ adultos: this.adultos });
     }
   };
+
   subtrairValor = (tipo: any) => {
     if (tipo === 'criancas') {
       this.criancas--;
@@ -117,5 +192,92 @@ export class AppComponent implements OnInit {
       this.adultos--;
       this.myForm.patchValue({ adultos: this.adultos });
     }
+
+    if (this.adultos < 0) {
+      this.adultos = 0;
+      this.myForm.patchValue({ adultos: this.adultos });
+    }
+
+    if (this.criancas < 0) {
+      this.criancas = 0;
+      this.myForm.patchValue({ criancas: this.criancas });
+    }
+  };
+
+  getDistance = (
+    originLatitude: number,
+    originLongitude: number,
+    destinationLatitude: number,
+    destinationLongitude: number
+  ) => {
+    const EARTH_RADIUS = 6_371.071; // Earth
+    const diffLatitudeRadians = this.degreesToRadians(
+      destinationLatitude - originLatitude
+    );
+    const diffLongitudeRadians = this.degreesToRadians(
+      destinationLongitude - originLongitude
+    );
+    const originLatitudeRadians = this.degreesToRadians(originLatitude);
+    const destinationLatitudeRadians =
+      this.degreesToRadians(destinationLatitude);
+    const kmDistance =
+      2 *
+      EARTH_RADIUS *
+      Math.asin(
+        Math.sqrt(
+          Math.sin(diffLatitudeRadians / 2) *
+            Math.sin(diffLatitudeRadians / 2) +
+            Math.cos(originLatitudeRadians) *
+              Math.cos(destinationLatitudeRadians) *
+              Math.sin(diffLongitudeRadians / 2) *
+              Math.sin(diffLongitudeRadians / 2)
+        )
+      );
+    return kmDistance;
+  };
+
+  degreesToRadians = (value: number) => {
+    return (value * Math.PI) / 180;
+  };
+
+  setPrecoViagem = () => {
+    let valorPassagemAdulto = 0;
+    let valorPassagemCrianca = 0;
+    if (
+      this.myForm.get('paisOrigem')?.value ===
+      this.myForm.get('paisDestino')?.value
+    ) {
+      console.log('1');
+      valorPassagemAdulto = this.distanciaViagem * 0.3;
+      valorPassagemCrianca = this.distanciaViagem * 0.15;
+    } else {
+      console.log('2');
+
+      valorPassagemAdulto = this.distanciaViagem * 0.5;
+      valorPassagemCrianca = this.distanciaViagem * 0.25;
+    }
+
+    if (this.myForm.get('classe')?.value === 'Executiva') {
+      console.log('3');
+
+      valorPassagemAdulto = valorPassagemAdulto * 1.8;
+      valorPassagemCrianca = valorPassagemCrianca * 1.4;
+    }
+
+    if (Number(this.myForm.get('milhas')?.value) > 0) {
+      console.log('4');
+
+      let valorAbatido = Number(this.myForm.get('milhas')?.value) * 0.02;
+      this.valorAbatidoMilhas = valorAbatido.toLocaleString();
+    }
+
+    this.valorAdulto = valorPassagemAdulto.toLocaleString();
+    this.valorCrianca = valorPassagemCrianca.toLocaleString();
+
+    this.totalViagem = (
+      this.adultos * valorPassagemAdulto +
+      this.criancas * valorPassagemCrianca -
+      this.valorAbatidoMilhas
+    ).toLocaleString();
   };
 }
